d3a952543db118edad91846394c7f1f37c55560f
Jyoti <jyothi@mobifyi.com>
2018-12-01 13:42:32 +0530

    Basic Progress completed commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as dayjs from 'dayjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'progress';
+export class AppComponent implements OnInit {
+  yearProgress: Number;
+  monthProgress: Number;
+  dayProgress: Number;
+  today: any;
+  now = dayjs();
+  ngOnInit() {
+    this.today = dayjs().format('dddd DD/MM/YYYY');
+    this.dayProgress = this.findProgress('day');
+    this.monthProgress = this.findProgress('month');
+    this.yearProgress = this.findProgress('year');
+  }
+  findProgress(a) {
+    const start = dayjs(this.now).startOf(a);
+    const end = dayjs(this.now).endOf(a);
+    const totalMillisInRange = end.valueOf() - start.valueOf();
+    const elapsedMillis = this.now.valueOf() - start.valueOf();
+    return Math.max(
+      0,
+      Math.min(100, 100 * (elapsedMillis / totalMillisInRange))
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-
+import {
+  MatToolbarModule,
+  MatCardModule,
+  MatProgressBarModule
+} from '@angular/material';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatProgressBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
